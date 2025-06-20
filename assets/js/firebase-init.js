d4c0dac38b9a6cb849ccea33b7431588b7729013
Swarnadeep17 <diip.brdhn@gmail.com>
2025-06-21 04:05:59 +0530

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,5 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head><title>TimeToBite</title></head>
-<body><h1>Coming Soon...</h1></body>
-</html>
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAU7VSaflcU9Rl7x0v3qixgVMv4TCn-40",
+  authDomain: "timetobite-633d8.firebaseapp.com",
+  projectId: "timetobite-633d8",
+  storageBucket: "timetobite-633d8.firebasestorage.app",
+  messagingSenderId: "110240199781",
+  appId: "1:110240199781:web:2fb811aeaf242de138028c",
+  measurementId: "G-SZN87MPS2V"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
